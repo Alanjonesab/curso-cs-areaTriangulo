3abbd2a1dbd23d841d1370f581efb9038338681d
Alanjonesab <alanjones_18@hotmail.com>
2020-07-23 10:42:19 -0300

    Primeira versao Area do triangulo

---
+++ AreaTriangulo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace AreaTriangulo {
+    class Program {
+        static void Main(string[] args) {
+
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double areaX = x.CalculaArea();
+            double areaY = y.CalculaArea();
+
+            Console.WriteLine("As área do triângulo X é {0} e a do triângulo Y é {1}", areaX, areaY);
+        }
+    }
+}
---
+++ AreaTriangulo/Triangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AreaTriangulo {
+    class Triangulo {
+
+        public double A;
+        public double B;
+        public double C;
+
+        public double CalculaArea() {
+            double area, p;
+
+            p = (A + B + C) / 2;
+            area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+            return area;
+            
+        }
+    }
+}
